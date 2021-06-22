2c7e07fe63c658928238f96fc8ee6224820fd34e
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-06-22 12:10:54 -0400

    Resonded to Vincent

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -8,6 +8,7 @@ namespace communication_log
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Jose!");
+            Console.WriteLine("Hello Vincent, we have established good comms check..Ready to relay message");
         }
     }
 }
