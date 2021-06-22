6db2b48d0b9d6bb314a6e5a3b95cca3de50900af
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-06-22 12:38:06 -0400

    Changed background color

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -6,9 +6,9 @@ namespace communication_log
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Blue;
             Console.WriteLine("Hello World!");
-            Console.BackgroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Hello Jose!");
             Console.WriteLine("Hello Vincent, we have established good comms check..Ready to relay message");
             Console.WriteLine("I read your Lima Charlie");
